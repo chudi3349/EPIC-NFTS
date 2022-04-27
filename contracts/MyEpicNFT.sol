f32541fa1bf7b64c35efc8affd5e4e23dc5907c3
chudi3349 <victorejiofor3349@gmail.com>
2022-04-27 11:50:21 +0100

    My first Commit just added few lines of code the run.js and the MyEpicNFT.sol

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+
+contract MyEpicNFT is ERC721URIStorage{
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  constructor() ERC721 ("SquareNFT", "SQUARE"){
+    console.log("Have you seen a man skillful at his work? He will stand before kings.");
+  }
+
+  function makeAnEpicNFT() public {
+    uint256 newItemId = _tokenIds.current();
+    _safeMint(msg.sender, newItemId);
+    _setTokenURI(newItemId, "blah");
+    _tokenIds.increment();
+  }
+}
+
+/**
+NUMBER: 1
+THIS WAS THE FIST CONTRACT 
+
+contract MyEpicNFT {
+  constructor(){
+    console.log("Have you seen a man skillful at his work? He will stand before kings.");
+  }
+} */
